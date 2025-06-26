a5ef409cc8159bd5c602d48f6443a59e4600159f
Vladyslav154 <v.d.mezentsev@gmail.com>
2025-06-26 16:51:01 +0200

    Add files via upload

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+import { AppLayout } from '@/components/layout/app-layout'
+import { DashboardContent } from '@/components/dashboard/dashboard-content'
+
+export default function DashboardPage() {
+  return (
+    <AppLayout>
+      <DashboardContent />
+    </AppLayout>
+  )
+}
