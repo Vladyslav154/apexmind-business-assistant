0fa98565b2fcc53660e52ec50adf33b39acb7afe
Vladyslav154 <v.d.mezentsev@gmail.com>
2025-06-26 21:08:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
-const path = require('path');
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+  outputFileTracingRoot: path.join(__dirname, '../'),
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || '',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'your-secret-key',
   },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
